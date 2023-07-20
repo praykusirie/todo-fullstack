efd7f16a6c96e20dcd8f4b7e3e869c046c70c475
prayjonas <prayjonas27@gmail.com>
2023-07-20 23:22:02 +0300

    adding database connection

--- server/server.js
+++ server/server.js
@@ -7,13 +7,30 @@ const morgan = require('morgan')
 const cors = require('cors')
 require("dotenv").config()
 
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
 // middleware
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app,use(bodyParser.json())
+app.use(bodyParser.json())
 
+// connecting with our mongodb database
+mongoose.connect(process.env.DATABASE, options)
 
-app.listen(process.env.PORT, () => console.log(`Server is runnign at port ${process.env.PORT}`))
+//Listening when we are connected in our database and if not getting noticable error
+mongoose.connection.on('open', () => {
+    try {
+        console.log('Connected to the mongodb')
+        app.listen(process.env.PORT, () => console.log(`Server is runnign at port ${process.env.PORT}`))
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
